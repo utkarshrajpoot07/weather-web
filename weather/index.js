22019f3695a07c2dbfcf90c683bcb4ce25a6d347
Utkarsh Rajpoot <156061027+utkarshrajpoot07@users.noreply.github.com>
2024-02-08 14:00:31 +0530

    Update index.js

--- weather/index.js
+++ weather/index.js
@@ -25,7 +25,7 @@ btn.addEventListener('click', function()
         city.innerHTML=`Weather of <span>${nameval}<span>`
         temp.innerHTML=`Temperature: <span>${convertion(temperature)} C<span>`
         description.innerHTML=`Sky Condition: <span>${descrip}<span>`
-        wind.innerHTML=`Wind Speed: <span>${wndspeed}<span>`
+        wind.innerHTML=`Wind Speed: <span>${wndspeed} km/h<span>`
 
     })
 
